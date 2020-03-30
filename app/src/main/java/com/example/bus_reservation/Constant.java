fe9caab3fae52127708e9f1968693f15bc6c6869
Bilal Tahir <bilalt586@gmail.com>
2020-03-30 12:32:26 +0500

    hello

--- app/src/main/java/com/example/bus_reservation/Constant.java
+++ app/src/main/java/com/example/bus_reservation/Constant.java
@@ -2,13 +2,16 @@ package com.example.bus_reservation;
 
 public class Constant {
 
-    public static final String Base_url_stripe = "http://marketist.eparking.website/Freelancer/provider_stripe.php?";
+    //https://providors.finalyearprojects.website
+    public static final String Base_url="https://providors.finalyearprojects.website/Freelancer/Api.php";
 
-    public static final String Base_url="http://marketist.eparking.website/Freelancer/Api.php";
+    public static final String Base_url_stripe = "https://providors.finalyearprojects.website/Freelancer/provider_stripe.php?";
 
-    public static final String chat_image_url = "http://marketist.eparking.website/uploads/chat/";
+//    public static final String Base_url="http://marketist.eparking.website/Freelancer/Api.php";
 
-    public static final String Base_url_get_dashboard = "http://marketist.eparking.website/Client/Api.php?providor_dashboard=1&providor_id=";
+    public static final String chat_image_url = "https://providors.finalyearprojects.website/uploads/chat/";
+
+    public static final String Base_url_get_dashboard = "https://providors.finalyearprojects.website/Client/Api.php?providor_dashboard=1&providor_id=";
 
     public static final String Base_url_get_chat=Base_url+"?get_chat=true&provider_id=";
 
@@ -20,9 +23,9 @@ public class Constant {
 
     public static final String Base_url_booking_view=Base_url+"?view_booking=true&booking_id=";
 
-    public static final String booking_image_url="http://marketist.eparking.website/uploads/booking/";
+    public static final String booking_image_url="https://providors.finalyearprojects.website/uploads/booking/";
 
-    public static final String Default_membership="http://marketist.eparking.website/Freelancer/Api.php?payments_settings=true";
+    public static final String Default_membership="https://providors.finalyearprojects.website/Freelancer/Api.php?payments_settings=true";
 
     public static final String Base_url_delete_gallery = Base_url+"?remove_gallery=true&image_id=";
 
@@ -40,9 +43,9 @@ public class Constant {
 
     public static final String Base_url_bookings=Base_url+"?booking_list=true&provider_id=";
 
-    public static final String Image_url="http://marketist.eparking.website/Client/";
+    public static final String Image_url="https://providors.finalyearprojects.website/Client/";
 
-    public static final String gallery_url="http://marketist.eparking.website/Freelancer/";
+    public static final String gallery_url="https://providors.finalyearprojects.website/Freelancer/";
 
     public static final String Banner_url = Base_url+"?slider=true";
 
